c0818cf019b59d07cd8213b804959dd1c745bfbb
Mayur M <71628364+mayurmahajan111@users.noreply.github.com>
2022-07-24 13:46:54 +0530

    Update calculator.cpp

--- calc/calculator.cpp
+++ calc/calculator.cpp
@@ -2,4 +2,31 @@
 using namespace std;
 int main(){
     int a,b;
+
+    cout<<"Enter the value of a"<<endl;
+    cin>>a;
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+
+    char op;
+    cout<<"enter the operation you want to perform"<<endl;
+    cin>>op;
+
+    switch (op){
+        case '+': cout <<(a+b)<<endl;
+          break;
+
+        case '-': cout << (a - b) <<endl;
+           break;
+            
+
+        case '*':cout << (a * b) <<endl;
+           break;
+
+        case '/':cout << (a / b) <<endl;
+           break;
+
+        case '%':cout << (a % b) <<endl;
+          break;
+    }
 }
